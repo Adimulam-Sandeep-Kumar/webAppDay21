43679421167d69dabe38416d590104205c7d16d9
Sandeep Kumar <sandeepkumar.adimulam@jobiak.com>
2022-05-17 11:09:39 +0530

    Adding Project

---
+++ webAppDay21/src/main/java/ai/jobiak/servlet/Hello.java
@@ -0,0 +1,35 @@
+package ai.jobiak.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class hello
+ */
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	for(int i=1;i<=5;i++) {
+			
+			System.out.println("Loop running..."+i);
+						
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
